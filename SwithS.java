226aef0425944d0e1c68d39cf6140f5b086b85ef
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-03-24 08:47:24 +0530

    Switch case

---
+++ SwithS.java
@@ -0,0 +1,37 @@
+public class SwithS 
+{
+    public static void main(String[] args) 
+    {
+        int y = 3;
+
+        switch (y) 
+        {
+            case 1:
+              System.out.println("Mon");
+              break;
+            case 2:
+              System.out.println("Tues");
+              break;
+            case 3:
+              System.out.println("Wens");
+              break;
+            case 4:
+              System.out.println("Thus");
+              break;
+            case 5:
+              System.out.println("Fri");
+              break;
+            case 6:
+              System.out.println("Sat");
+              break;
+            case 7:
+              System.out.println("sun");
+              break;
+            default:
+              System.out.println("Enter a valid number");
+           
+            //when we use break key word it means that  onece the case is matches just come out of that block
+        }
+        
+    }
+}
