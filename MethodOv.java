cd5cd9b5e68eae11813430d2e8a094ecae3bb590
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-03-25 07:24:36 +0530

    Methor_Overloading

---
+++ MethodOv.java
@@ -0,0 +1,30 @@
+class Calculator
+    {
+        //this add only take two parameters
+        public int add(int num1, int num2)
+        {
+            return num1 + num2;
+        } 
+
+        //this add only take three parameters
+        public int add(int num1, int num2 ,int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        //in methord Overloading there are different number of paramiters in same method or same number of parameters different types using same name 
+    }
+    
+
+public class MethodOv 
+{
+    public static void main(String[] args) 
+    {
+
+        Calculator obj = new Calculator();
+        int r1 = obj.add(4, 7);
+        System.out.println(r1);
+
+    
+    }
+}
